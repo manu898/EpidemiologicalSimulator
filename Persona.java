e187822a1dca9f3049d146b7420bd579db48302d
Manuel Caparrelli <manuel.caparrelli@gmail.com>
2020-04-29 18:50:16 +0200

    nulla

---
+++ Persona.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Persona {
+
+    public int ID;
+
+    Coppia posizione;
+
+    public boolean movimento;
+
+    public boolean inCura;
+
+    ArrayList<Persona> incontrate;
+
+    public Persona(int ID, int y, int x){
+        this.ID = ID;
+        posizione = new Coppia(y, x);
+        incontrate = new ArrayList<Persona>();
+    }
+
+    public void azzera(){
+
+        incontrate.clear();
+
+    }
+
+
+
+
+
+}
