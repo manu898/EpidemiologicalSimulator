2c3480772ccedc716e57a59f8edc8c2d1cceea8a
Manuel Caparrelli <manuel.caparrelli@gmail.com>
2020-04-29 21:21:37 +0200

    nulla

---
+++ src/Coppia.java
@@ -0,0 +1,18 @@
+public class Coppia{
+
+    public int x;
+    public int y;
+
+    public Coppia(int y, int x){
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Test {
+
+    public void cura(Persona p){
+
+    }
+
+    public void incontra(Persona p, Persona q){
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
