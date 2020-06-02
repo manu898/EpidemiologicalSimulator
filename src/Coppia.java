7427edb43f3362059274223efd0e02b5a8bd4ebd
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-06-02 18:29:47 +0200

    modifica di Coppia.java

--- src/Coppia.java
+++ src/Coppia.java
@@ -1,13 +1,15 @@
 public class Coppia{
+//un oggetto di tipo Coppia rappresenta una coppia di interi
+//viene sfruttato per tenere conto della posizione di una persona nell'arena
 
-    public int y;
-    public int x;
+    private int y;
+    private int x;
 
     public Coppia(int y, int x){
         this.y = y;
         this.x = x;
     }
-
+//getter
     public int getX() {
         return x;
     }
@@ -15,4 +17,9 @@ public class Coppia{
     public int getY() {
         return y;
     }
+
+//setter
+    public void setX(int x) { this.x = x;}
+
+    public void setY(int y) {this.y = y;}
 }
