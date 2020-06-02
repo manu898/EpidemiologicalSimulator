b2fbf94ba9edaead0feb66a6fb24cfde45957ddf
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-06-02 18:46:49 +0200

    modifica di Coppia.java

--- src/Cella.java
+++ src/Cella.java
@@ -2,22 +2,30 @@ import java.util.ArrayList;
 
 public class Cella {
 	//accesso?
-	public ArrayList<Persona> fila = new ArrayList<>();
+	//la lista effettiva delle persone in una cella
+	private ArrayList<Persona> fila = new ArrayList<>();
 
+	//prendi la persona dalla fila in base alla posizione in essa
 	public Persona pos_get (int pos) {
 		return fila.get(pos);
 	}
 
+	//si potrebbe fare come input la persona stessa invece del suo ID?
+	//prendi la persona dalla fila in base al suo ID
 	public Persona id_get (int id) throws PersonNotFoundException {
 		for (int i = 0; i < fila.size(); i++) {
+			//sostituisci ID con getID()
 			if ( fila.get(i).ID == id )
 				return fila.get(i);
 		}
 		throw new PersonNotFoundException();	
 	}
 
+	//si potrebbe fare come input la persona stessa invece del suo ID?
+	// rimuovi la persona dalla fila in base al suo id
 	public void remove (int id) throws PersonNotFoundException {
 		for (int i = 0; i < fila.size(); i++) {
+			//sostituisci ID con getID()
 			if ( fila.get(i).ID == id )
 				fila.remove(i);
 				return;
@@ -25,10 +33,12 @@ public class Cella {
 		throw new PersonNotFoundException();
 	}
 
+	//aggiungi in fondo alla fila una persona
 	public void add(Persona p) {
 		fila.add(p);
 	}
 
+	//ritorna la dimensione della fila
 	public int size() {
 		return fila.size();
 	}
