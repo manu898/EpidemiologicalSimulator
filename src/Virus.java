25aaa99fd53d325d8add57e26a3a42f0fe930093
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-06-03 11:29:27 +0200

    aggiunta classe Virus con campi principali e relativi setter/getter; campi Persona resi private

--- src/Virus.java
+++ src/Virus.java
@@ -1,11 +1,30 @@
 public class Virus {
 
+    //parametri della simulazione
+    //vanno aggiunti i controlli sulla correttezza dei parametri
     private static int INFETTIVITA;
     private static int LETALITA;
     private static int SINTOMATICITA;
     private static int DURATA;
 
+    //campi dell'oggetto virus
+    private int giornoDadoS;  //controlla: valore compreso tra giorno odierno e giorno odierno+(D/3)
+    private int giornoDadoM;  //controlla: valore compreso tra giorno odierno e giorno odierno+D
+
     //static getter
-    public static getI() { return Virus.INFETTIVITA; }
-    public static getL() { return Virus.LETALITA; }
+    public static int getI() { return Virus.INFETTIVITA; }
+    public static int getL() { return Virus.LETALITA; }
+    public static int getS() { return Virus.SINTOMATICITA; }
+    public static int getD() { return Virus.DURATA; }
+
+    //getter
+    public int getGiornoDadoS() { return giornoDadoS; } //necessario?
+    public int getGiornoDadoM() { return giornoDadoM; } //necessario?
+
+    //static setter
+    //vanno aggiunti i controlli sulla correttezza dei parametri
+    public static void setI(int i) { Virus.INFETTIVITA = i; }
+    public static void setL(int l) { Virus.LETALITA = l; }
+    public static void setS(int s) { Virus.SINTOMATICITA = s; }
+    public static void setD(int d) { Virus.DURATA = d; }
 }
