9c2f61a1dcc0a5e4a8a6e8f44a100a9eb9752a71
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-06-04 12:07:12 +0200

    Classe Virus aggiornata con i principali metodi e attributi (se ne servono altri si vedrà). Da controllare la correttezza dei valori degli attributi e dei parametri di input e l'eventuale gestione delle eccezioni

--- src/Virus.java
+++ src/Virus.java
@@ -10,19 +10,28 @@ public class Virus {
     private static int DURATA;
 
     //campi dell'oggetto virus
-    private Random r;
+    private static Random r = new Random();
     private int giornoContagio;
     private int giornoDadoS;  //controlla: valore compreso tra giorno odierno e giorno odierno+(D/3)
     private int giornoDadoM;  //controlla: valore compreso tra giorno odierno e giorno contagio+D
 
     public Virus() {
         this.giornoContagio = Universo.getGiorno();
-        this.r = new Random();
+    }
+
+    public static boolean dadoContagio() {
+        if ( r.nextInt(101) <= INFETTIVITA)
+            return true;
+        return false;
     }
 
     public boolean isIncubazioneFinita() {
-        if (Universo.getGiorno() == giornoContagio + Virus.DURATA / 6)
+        if (Universo.getGiorno() == giornoContagio + DURATA / 6) {
+            int bound = (giornoContagio + DURATA / 3) - Universo.getGiorno();
+            giornoDadoS = Universo.getGiorno() + r.nextInt(bound);
+            //e se il giornoDadoS risulta lo stesso del giorno di fine incubazione poiché r.nextInt(bound) ha tornato 0?
             return true;
+        }
         return false;
     }
 
@@ -38,23 +47,38 @@ public class Virus {
         return false;
     }
 
+    public boolean isMalattiaFinita() {
+        if (Universo.getGiorno() == giornoContagio + DURATA)
+            return true;
+        return false;
+    }
+
+
     public boolean dadoS() {
         int x = r.nextInt(101);
-        if (x <= Virus.SINTOMATICITA) {
-            //controlla: valore giornoDadoM compreso tra giorno odierno e giorno contagio+D (escluso)
-            int bound = (giornoContagio + Virus.DURATA) - giornoDadoS;
+        if (x <= SINTOMATICITA) {
+            //controlla: valore giornoDadoM compreso tra giorno odierno(giornoDadoS) e giorno contagio+D (escluso)
+            int bound = (giornoContagio + DURATA) - giornoDadoS;
             giornoDadoM = Universo.getGiorno() + r.nextInt(bound);
+            //e se il giornoDadoM risulta lo stesso del giornoDadoS poiché r.nextInt(bound) ha tornato 0?
             return true;
         }
         return false;
     }
 
+    public boolean dadoM() {
+        int x = r.nextInt(101);
+        if (x <= LETALITA)
+            return true;
+        return false;
+    }
+
 
     //static getter
-    public static int getI() { return Virus.INFETTIVITA; }
-    public static int getL() { return Virus.LETALITA; }
-    public static int getS() { return Virus.SINTOMATICITA; }
-    public static int getD() { return Virus.DURATA; }
+    public static int getI() { return INFETTIVITA; }
+    public static int getL() { return LETALITA; }
+    public static int getS() { return SINTOMATICITA; }
+    public static int getD() { return DURATA; }
 
     //getter
     public int getGiornoDadoS() { return giornoDadoS; } //necessario?
@@ -62,10 +86,10 @@ public class Virus {
 
     //static setter
     //vanno aggiunti i controlli sulla correttezza dei parametri
-    public static void setI(int i) { Virus.INFETTIVITA = i; }
-    public static void setL(int l) { Virus.LETALITA = l; }
-    public static void setS(int s) { Virus.SINTOMATICITA = s; }
-    public static void setD(int d) { Virus.DURATA = d; }
+    public static void setI(int i) { INFETTIVITA = i; }
+    public static void setL(int l) { LETALITA = l; }
+    public static void setS(int s) { SINTOMATICITA = s; }
+    public static void setD(int d) { DURATA = d; }
 
     //setter
     //controlla: valore compreso tra giorno odierno e giorno odierno+(D/3)
