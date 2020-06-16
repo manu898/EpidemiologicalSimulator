911cff2b191f686e2b247bc8ce93eaf66b83da0d
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-06-16 15:30:27 +0200

    Scritto il metodo run di simulazione e alcune cose di supporto, tutto da testare

--- src/Simulazione.java
+++ src/Simulazione.java
@@ -9,6 +9,14 @@ public class Simulazione {
     private Arena arena;
     private double R0;
 
+    private double perc_mov;
+    //potrebbero essere solo variabili locali nel metodo run
+    private int verdi_sani;
+    private int gialli;
+    private int rossi;
+    private int neri;
+    private int blu;
+
 
     public Simulazione( Governo governo, Arena arena, int popolazione, double velocita ) {
         giorno = 1;
@@ -26,10 +34,67 @@ public class Simulazione {
         //TEST
         arena.distribuisciPersone(persone);
         this.velocita = velocita;
-        R0 = velocita * Virus.getD() * Virus.getI();
+        R0 = velocita * Virus.getD() * Virus.getI(); //TEST
+        perc_mov = velocita * 100 / popolazione;   //TEST
     }
 
-    //public boolean run() {} TODO
+    public boolean run(int giorni) {     //TEST
+        for (int i = 0; i < giorni ; i++) {
+            verdi_sani = 0;
+            gialli = 0;
+            rossi = 0;
+            neri = 0;
+            blu = 0;
+            int in_movimento = 0;
+            for (Persona p : persone) {
+                if (p.getMovimento())
+                    in_movimento++;
+            }
+            velocita = in_movimento * perc_mov / 100;
+            int n_incontrate = 0;
+            while (n_incontrate / in_movimento < velocita) {
+                arena.move(persone);
+                n_incontrate += arena.check_incontri();
+            }
+            for (Persona p: persone) {
+                p.checkVirus();
+                check_stato(p);
+            }
+            //governo.controlla()
+            if (risorse_finite() || vittoria_malattia() || (verdi_sani + blu + neri == getPopolazione()) )
+                return false;
+        }
+        return true;
+    }
+
+    public boolean vittoria_malattia() {   //TEST
+        return (neri == getPopolazione());
+    }
+
+    public boolean risorse_finite() {     //TEST
+        return (governo.getRisorse() <= 0);
+    }
+
+    private void check_stato(Persona p) {  //TEST
+        switch (p.getStato()) {
+            case VERDE:
+                if (p.getVir() != null)
+                    verdi_sani++;
+                break;
+            case GIALLO:
+                gialli++;
+                break;
+            case ROSSO:
+                rossi++;
+                break;
+            case NERO:
+                neri++;
+                break;
+            case BLU:
+                blu++;
+                break;
+        }
+    }
 
     private void init_persone(int popolazione) {
         for (int i = 0; i < popolazione; i++) {
@@ -56,6 +121,19 @@ public class Simulazione {
     public Arena getArena() { return arena; }
 
     public double getR0() { return R0; }
+
+    public double getPerc_mov() { return perc_mov; }
+
+    public int getVerdi_sani() { return verdi_sani; }
+
+    public int getGialli() { return gialli; }
+
+    public int getRossi() { return rossi; }
+
+    public int getNeri() { return neri; }
+
+    public int getBlu() { return blu; }
+
     //setter
     public void setGiorno(int g) { giorno = g; }
 
@@ -70,4 +148,16 @@ public class Simulazione {
     public void setArena(Arena arena) { this.arena = arena; }
 
     public void setR0(double R0) { this.R0 = R0; }
+
+    public void setPerc_mov(double perc_mov) { this.perc_mov = perc_mov; }
+
+    public void setVerdi_sani(int verdi_sani) { this.verdi_sani = verdi_sani; }
+
+    public void setGialli(int gialli) { this.gialli = gialli; }
+
+    public void setRossi(int rossi) { this.rossi = rossi; }
+
+    public void setNeri(int neri) { this.neri = neri; }
+
+    public void setBlu(int blu) { this.blu = blu; }
 }
