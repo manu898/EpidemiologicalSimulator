0f8962d55663190812dcfd553c318b7ac0601cf7
Manuel Caparrelli <manuel.caparrelli@gmail.com>
2020-06-18 13:30:04 +0200

    Riordinata la classe UiJfx

--- src/UiJfx.java
+++ src/UiJfx.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 
 public class UiJfx extends Application {
 
-
     public Stage window;
 
     // scena iniziale - Inserimento parametri
@@ -24,31 +23,60 @@ public class UiJfx extends Application {
     private Label arenaLLabel = new Label("ArenaL");
     private TextField arenaL = new TextField();
 
+    private HBox arenaBox = new HBox(arenaHLabel,arenaH,arenaLLabel,arenaL);
+
     private Button btnInvia = new Button("Inizia simulazione");
+
 
     private Label popolazioneLabel = new Label("Popolazione");
     private TextField popolazione = new TextField();
+    private HBox popolaioneBox = new HBox(popolazioneLabel,popolazione);
+
 
     private Label velocitaLabel = new Label("Velocita");
     private TextField velocita = new TextField();
+    private HBox velocitaBox = new HBox(velocitaLabel,velocita);
+
 
     private Label durataLabel = new Label("Durata");
     private TextField durata = new TextField();
+    private HBox durataBox = new HBox(durataLabel,durata);
+
 
     private Label tamponeLabel = new Label("Tampone");
     private TextField tampone = new TextField();
+    private HBox tamponeBox = new HBox(tamponeLabel,tampone);
+
 
     private Label risorseLabel = new Label("Risorse");
     private TextField risorse = new TextField();
+    private HBox risorseBox = new HBox(risorseLabel,risorse);
+    private final Tooltip tooltipRisorse = new Tooltip("Le risorse devono rispettare i seguenti vincoli");
+
 
     private Label infettivitaLabel = new Label("Infettività");
     private TextField infettivita = new TextField();
+    private HBox infettivitaBox = new HBox(infettivitaLabel,infettivita);
+
 
     private Label sintomaticitaLabel = new Label("Sintomaticità");
     private TextField sintomaticita = new TextField();
+    private HBox sintomaticitaBox = new HBox(sintomaticitaLabel,sintomaticita);
+
 
     private Label letalitaLabel = new Label("Letalità");
     private TextField letalita = new TextField();
+    private HBox letalitaBox = new HBox(letalitaLabel,letalita);
+
+    private RadioButton strg1 = new RadioButton("Strategia 1");
+    private RadioButton strg2 = new RadioButton("Strategia 2");
+    private RadioButton strg3 = new RadioButton("Strategia 3");
+    private RadioButton strg4 = new RadioButton("Strategia 4");
+    private RadioButton selected = null;
+
+    ToggleGroup toggleGroup = new ToggleGroup();
+
+    private HBox strategieBox = new HBox();
 
     private static Simulazione simulazioneVera = null;
 
@@ -92,23 +120,47 @@ public class UiJfx extends Application {
         return velocita;
     }
 
-    public static void setSimulazione(Simulazione simulazione) {
-        simulazioneVera = simulazione;
-    }
-
-    public Simulazione getSimulazione() {
-        return simulazioneVera;
-    }
-
     public Alert alert = new Alert(Alert.AlertType.ERROR);
 
-    private Scene scene = null;
+    private Scene sceneIniziale = null;
 
-    private VBox vBox = null;
+    private VBox vBox = new VBox();
+
+    private final FlowPane container = new FlowPane();
+
+    private ScrollPane sc = new ScrollPane();
 
 
 
-    // sceneChart
+
+    // scena intermedia - interrompi
+
+    private Label fraseMid = new Label(" Sto simulando ...");
+
+    private Button btnInterrompi = new Button("Interrompi simulazione");
+
+    private Scene sceneMid = null;
+
+    private VBox vBoxMid = null;
+
+
+
+
+
+    // scena intermedia - finale
+
+    private Label fraseFinale = new Label("Simulazione finita ! Vedi le statistiche ");
+
+    private Button btnFinale = new Button("Vedi statistiche");
+
+    private Scene sceneFinale = null;
+
+    private VBox vBoxFinale = null;
+
+
+
+
+    // scena finale - sceneChart
 
     NumberAxis xAxis = new NumberAxis();
 
@@ -129,27 +181,6 @@ public class UiJfx extends Application {
     private VBox vBoxChart = null;
 
 
-    // scena intermedia - interrompi
-
-    private Label fraseMid = new Label(" Sto simulando ...");
-
-    private Button btnInterrompi = new Button("Interrompi simulazione");
-
-    private Scene sceneMid = null;
-
-    private VBox vBoxMid = null;
-
-    // scena intermedia - finale
-
-    private Label fraseFinale = new Label("Simulazione finita ! Vedi le statistiche ");
-
-    private Button btnFinale = new Button("Vedi statistiche");
-
-    private Scene sceneFinale = null;
-
-    private VBox vBoxFinale = null;
-
-
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -158,20 +189,36 @@ public class UiJfx extends Application {
 
         // scena iniziale - Inserimento parametri
 
-        vBox = new VBox();
+
+        btnInvia.setTooltip(tooltipRisorse);
+
+
+        setFont(20.0, arenaHLabel,arenaLLabel,popolazioneLabel,
+                velocitaLabel,risorseLabel,durataLabel,tamponeLabel,infettivitaLabel,sintomaticitaLabel,letalitaLabel);
+
+        setWidth(arenaH,arenaL,popolazione,risorse,velocita,durata,tampone,infettivita,sintomaticita,letalita,btnInvia);
+
+        strg1.setToggleGroup(toggleGroup);
+        strg2.setToggleGroup(toggleGroup);
+        strg3.setToggleGroup(toggleGroup);
+        strg4.setToggleGroup(toggleGroup);
+
+        strategieBox.getChildren().addAll(strg1,strg2,strg3,strg4);
+
+        setPosAndMargin(arenaBox,popolaioneBox,risorseBox,velocitaBox,tamponeBox,durataBox,infettivitaBox,sintomaticitaBox,letalitaBox,strategieBox);
+
+
+
+        /*vBox.getChildren().addAll(arenaLLabel,arenaL,arenaHLabel,arenaH,popolazioneLabel,popolazione,risorseLabel,risorse,velocitaLabel,velocita,
+                tamponeLabel,tampone,durataLabel,durata,
+                infettivitaLabel,infettivita,sintomaticitaLabel,sintomaticita,letalitaLabel,letalita,btnInvia);*/
+
+        vBox.getChildren().addAll(arenaBox,popolaioneBox,risorseBox,velocitaBox,tamponeBox,durataBox,infettivitaBox,sintomaticitaBox,letalitaBox,strategieBox,btnInvia);
 
         vBox.setAlignment(Pos.CENTER);
 
-        scene = new Scene(vBox);
+        sceneIniziale = new Scene(vBox);
 
-        setFont(20.0, arenaHLabel,arenaHLabel,popolazioneLabel,
-                velocitaLabel,durataLabel,tamponeLabel,infettivitaLabel,sintomaticitaLabel,letalitaLabel);
-
-        setWidthAndMargin(arenaH,arenaL,popolazione,risorse,velocita,durata,tampone,infettivita,sintomaticita,letalita,btnInvia);
-
-
-        vBox.getChildren().addAll(arenaHLabel,arenaH,arenaLLabel,arenaL,popolazioneLabel,popolazione,risorseLabel,risorse,velocitaLabel,velocita,durataLabel,durata,tamponeLabel,tampone,
-                infettivitaLabel,infettivita,sintomaticitaLabel,sintomaticita,letalitaLabel,letalita,btnInvia);
 
         btnInvia.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -199,7 +246,6 @@ public class UiJfx extends Application {
 
 
 
-
         // scene intermedia - Interrompi
 
         vBoxMid = new VBox();
@@ -207,6 +253,8 @@ public class UiJfx extends Application {
         vBoxMid.setAlignment(Pos.CENTER);
 
         sceneMid = new Scene(vBoxMid);
+
+        vBoxMid.getChildren().addAll(fraseMid,btnInterrompi);
 
         btnInterrompi.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -227,6 +275,8 @@ public class UiJfx extends Application {
 
         sceneFinale = new Scene(vBoxFinale);
 
+        vBoxFinale.getChildren().addAll(fraseFinale,btnFinale);
+
         btnFinale.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -234,8 +284,6 @@ public class UiJfx extends Application {
                 window.setFullScreen(true);
             }
         });
-
-
 
 
         // scena finale - Chart
@@ -285,10 +333,12 @@ public class UiJfx extends Application {
         stackedAreaChart.getData().addAll(morti,asintomatici,sintomatici,guariti);
 
 
+
+
         // Stage - inizialmente visualizziamo la scena iniziale di inserimento parametriì
 
         stage.setFullScreen(true);
-        stage.setScene(scene);
+        stage.setScene(sceneIniziale);
         stage.setTitle("Epidemiological simulator");
         stage.show();
 
@@ -300,12 +350,19 @@ public class UiJfx extends Application {
             label.setFont(new Font(20.0));
     }
 
-    public void setWidthAndMargin(Region... element){
+    public void setWidth(Region... element){
         for(Region node : element){
             node.setMaxWidth(100.0);
-            VBox.setMargin(node, new Insets(20));
         }
     }
+
+    public void setPosAndMargin(HBox... elements){
+        for(HBox element : elements){
+            element.setAlignment(Pos.CENTER);
+            VBox.setMargin(element, new Insets(10.0));
+        }
+    }
+
 
     public void addSeries(XYChart.Series series, ArrayList<Coppia> coppie){
         for(Coppia coppia : coppie){
@@ -314,8 +371,11 @@ public class UiJfx extends Application {
     }
 
     public boolean inviaDati() throws NumberFormatException{
+
         alert.setTitle("ERRORE");
+
         try{
+
             int arenaH_value = Integer.parseInt(getArenaH().getText());
             int arenaL_value = Integer.parseInt(getArenaL().getText());
             int popolazione_value = Integer.parseInt(getPopolazione().getText());
@@ -326,6 +386,9 @@ public class UiJfx extends Application {
             int infettivita_value = Integer.parseInt(getInfettivita().getText());
             int sintomaticita_value = Integer.parseInt(getSintomaticita().getText());
             int letalita_value = Integer.parseInt(getLetalita().getText());
+
+            selected = (RadioButton) toggleGroup.getSelectedToggle();
+
 
             // vincoli sull'arena
             if(arenaH_value <= 0 || arenaL_value <= 0){
@@ -404,7 +467,10 @@ public class UiJfx extends Application {
                 Integer.parseInt(getPopolazione().getText()),
                 Double.parseDouble(getVelocita().getText()));
 
+        System.out.println(selected);
+
         return true;
+
     }
 
 
