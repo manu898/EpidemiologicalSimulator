0a8653d86dbc4391ca7fb397ded0d615d4e3928d
Ico98 <lauletta.1811516@studenti.uniroma1.it>
2020-07-13 22:50:14 +0200

    commentate Cella e Coppia

--- src/Cella.java
+++ src/Cella.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class Cella {
-	//accesso?
+
 	//la lista effettiva delle persone in una cella
 	private ArrayList<Persona> fila = new ArrayList<Persona>();
 
-	//prendi la persona dalla fila in base alla posizione in essa
+	//prende la persona dalla fila in base alla posizione in essa
 	public Persona pos_get (int pos) {
 		return fila.get(pos);
 	}
 
-	//prendi la persona dalla fila in base al suo ID
+	//prende la persona dalla fila in base al suo ID
 	public Persona id_get (int id) {
 		for (int i = 0; i < fila.size(); i++) {
 			if ( fila.get(i).getID() == id )
@@ -19,14 +19,14 @@ public class Cella {
 		throw new PersonNotFoundException("la persona con l'id indicato non esiste nella cella");
 	}
 
-	// rimuovi la persona dalla fila
+	//rimuove la persona dalla fila
 	public void remove (Persona p) {
 		boolean ret = fila.remove(p);
 		if (ret == false)
 			throw new PersonNotFoundException("la persona indicata non esiste nella cella");
 	}
 
-	//aggiungi in fondo alla fila una persona
+	//aggiunge in fondo alla fila una persona
 	public void add(Persona p) {
 		fila.add(p);
 	}
